b0b67c12d8159fc0b02b979264cfb89706917c84
Frincu Ioan <ioan.frincu@yahoo.com>
2023-04-05 00:51:23 +0300

    commit 5

--- a1/a1.c
+++ a1/a1.c
@@ -8,9 +8,17 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #pragma pack(1)
+#define size_citire 512
 
+enum
+{
+    MAGIC,
+    VERSION,
+    SECT_NR,
+    TYPES,
+    PATH,
+};
 typedef struct LIST
 {
     // optiuni
@@ -34,53 +42,23 @@ typedef struct LIST
     char *path_string;
 } list;
 
-typedef struct SECTION{
+typedef struct SECTION
+{
     char name[12];
     unsigned short type;
     unsigned int offset;
     unsigned int size;
-}section;
+} section;
 
-typedef struct HEADER{
+typedef struct HEADER
+{
+    int corupted;
     unsigned char magic;
     unsigned short header_size;
     unsigned char version;
     unsigned char no_of_sections;
-    section* sectiuni;
-}header;
-
-void print_arg(list *l)
-{
-    printf("list: %d, parse: %d, extract: %d, findall: %d, path:%d\n", l->list, l->parse, l->extract, l->findall, l->path);
-    if (l->path)
-    {
-        printf("%s\n", l->path_string);
-    }
-    if (l->list)
-    {
-        printf("recurvive: %d, name_starts: %d, size_greater: %d\n", l->recursiv, l->name_start, l->greater_size);
-        if (l->name_start)
-        {
-            printf("string: %s\n", l->string);
-        }
-        if (l->greater_size)
-        {
-            printf("size: %d\n", l->size);
-        }
-    }
-    if (l->extract)
-    {
-        printf("section: %d, line: %d\n", l->section, l->line);
-        if (l->section)
-        {
-            printf("section_nr: %d\n", l->section_nr);
-        }
-        if (l->line)
-        {
-            printf("line_nr: %d\n", l->line_nr);
-        }
-    }
-}
+    section *sectiuni;
+} header;
 
 list *parsareArgumente(int n, char **arg)
 {
@@ -138,7 +116,7 @@ list *parsareArgumente(int n, char **arg)
     return l;
 }
 
-int listare(char *path, list *l, int ok)
+void listare(char *path, list *l, int ok)
 {
     DIR *dir = NULL;
     struct dirent *entry = NULL;
@@ -147,7 +125,8 @@ int listare(char *path, list *l, int ok)
     char filePath[512];
     if (dir == NULL)
     {
-        return -1;
+        printf("ERROR\ninvalid directory path\n");
+        return;
     }
     if (ok == 1)
     {
@@ -156,154 +135,287 @@ int listare(char *path, list *l, int ok)
     }
     while ((entry = readdir(dir)) != NULL)
     {
-        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
         {
-            sprintf(filePath, "%s/%s", path, entry->d_name);
-            if (lstat(filePath, &statbuf) == 0)
+            continue;
+        }
+        sprintf(filePath, "%s/%s", path, entry->d_name);
+        if (lstat(filePath, &statbuf) == 0)
+        {
+            if (l->recursiv)
             {
-                if (l->recursiv) // daca e recursiv
+                if (S_ISDIR(statbuf.st_mode))
                 {
-                    if (l->greater_size)
-                    {
-                        if (l->name_start)
-                        {
-                            if (statbuf.st_size > l->size && strncmp(entry->d_name, l->string, strlen(l->string)) == 0 && S_ISREG(statbuf.st_mode))
-                            {
-                                printf("%s\n", filePath);
-                            }
-                            if (S_ISDIR(statbuf.st_mode))
-                            {
-                                listare(filePath, l, ok);
-                            }
-                        }
-                        else
-                        {
-                            if (statbuf.st_size > l->size && S_ISREG(statbuf.st_mode))
-                            {
-                                printf("%s\n", filePath);
-                            }
-                            if (S_ISDIR(statbuf.st_mode))
-                            {
-                                listare(filePath, l, ok);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (l->name_start)
-                        {
-                            if (strncmp(entry->d_name, l->string, strlen(l->string)) == 0)
-                            {
-                                printf("%s\n", filePath);
-                            }
-                            if (S_ISDIR(statbuf.st_mode))
-                            {
-                                listare(filePath, l, ok);
-                            }
-                        }
-                        else
-                        {
-                            printf("%s\n", filePath);
-                            if (S_ISDIR(statbuf.st_mode))
-                            {
-                                listare(filePath, l, ok);
-                            }
-                        }
-                    }
+                    listare(filePath, l, ok);
                 }
-                else
+            }
+            if (l->name_start && strncmp(entry->d_name, l->string, strlen(l->string)) != 0)
+            {
+                continue;
+            }
+            if (l->greater_size && S_ISREG(statbuf.st_mode) && statbuf.st_size <= l->size)
+            {
+                continue;
+            }
+            if (l->greater_size)
+            {
+                if (!S_ISDIR(statbuf.st_mode))
                 {
-                    if (l->greater_size)
-                    {
-                        if (l->name_start)
-                        {
-                            if (statbuf.st_size > l->size && strncmp(entry->d_name, l->string, strlen(l->string)) == 0 && S_ISREG(statbuf.st_mode))
-                            {
-                                printf("%s\n", filePath);
-                            }
-                        }
-                        else
-                        {
-                            if (statbuf.st_size > l->size && S_ISREG(statbuf.st_mode))
-                            {
-                                printf("%s\n", filePath);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (l->name_start)
-                        {
-                            if (strncmp(entry->d_name, l->string, strlen(l->string)) == 0)
-                            {
-                                printf("%s\n", filePath);
-                            }
-                        }
-                        else
-                        {
-                            printf("%s\n", filePath);
-                        }
-                    }
+                    printf("%s\n", filePath);
                 }
+            }
+            else
+            {
+                printf("%s\n", filePath);
             }
         }
     }
     closedir(dir);
-    return 0;
 }
 
-void parsare(list* l){
-    int fd = open(l->path_string, O_RDONLY);
-    if(fd == -1){
-        printf("ERROR\ninvalid directory path\n");
-        return;
+header *parsare(char *path, int *eroare)
+{
+    int fd = open(path, O_RDONLY);
+    if (fd == -1)
+    {
+        *eroare = PATH;
+        return NULL;
     }
-    header* h = (header*)malloc(1* sizeof(header));
+    header *h = (header *)malloc(1 * sizeof(header));
     lseek(fd, -3, SEEK_END);
     read(fd, &h->header_size, 2);
     read(fd, &h->magic, 1);
-    if(h->magic != '0'){
-        printf("ERROR\nwrong magic\n");
+    if (h->magic != '0')
+    {
+        *eroare = MAGIC;
+        close(fd);
         free(h);
-        return;
+        return NULL;
     }
-
-    lseek(fd, -h->header_size, SEEK_END);
-    read(fd, &h->version, 1);
-    if(h->version < 72 || h->version > 156){
-        printf("ERROR\nwrong version\n");
+    lseek(fd, 0, SEEK_SET);
+    int size = lseek(fd, 0, SEEK_END);
+    if (h->header_size > size)
+    {
+        close(fd);
         free(h);
-        return;
+        return NULL;
+    }
+    lseek(fd, -(h->header_size), SEEK_END);
+    read(fd, &h->version, 1);
+    if (h->version < 72 || h->version > 156)
+    {
+        *eroare = VERSION;
+        close(fd);
+        free(h);
+        return NULL;
     }
     read(fd, &h->no_of_sections, 1);
-    if(h->no_of_sections < 3 || h->no_of_sections > 19){
-        printf("ERROR\nwrong sect_nr\n");
+    if (h->no_of_sections < 3 || h->no_of_sections > 19)
+    {
+        *eroare = SECT_NR;
+        close(fd);
         free(h);
-        return;
+        return NULL;
     }
-    h->sectiuni = (section*)malloc(h->no_of_sections*sizeof(section));
-    for(int i=0;i<h->no_of_sections;i++){
+    h->sectiuni = (section *)malloc(h->no_of_sections * sizeof(section));
+    for (int i = 0; i < h->no_of_sections; i++)
+    {
         read(fd, h->sectiuni[i].name, 11);
         read(fd, &h->sectiuni[i].type, 2);
         read(fd, &h->sectiuni[i].offset, 4);
         read(fd, &h->sectiuni[i].size, 4);
-        if(h->sectiuni[i].type == 57 || h->sectiuni[i].type == 63 || h->sectiuni[i].type == 15 || h->sectiuni[i].type == 29){
+        if (h->sectiuni[i].type == 57 || h->sectiuni[i].type == 63 || h->sectiuni[i].type == 15 || h->sectiuni[i].type == 29)
+        {
             continue;
-        }else{
-            printf("ERROR\nwrong sect_types\n");
+        }
+        else
+        {
+            *eroare = TYPES;
+            close(fd);
             free(h->sectiuni);
             free(h);
-            return;
+            return NULL;
         }
     }
+    close(fd);
+    return h;
+}
+
+void print_header(header *h)
+{
     printf("SUCCESS\nversion=%d\nnr_sections=%d\n", h->version, h->no_of_sections);
-    for(int i=0;i<h->no_of_sections;i++){
-        printf("section%d: %s %d %d\n", i+1, h->sectiuni[i].name, h->sectiuni[i].type, h->sectiuni[i].size);
+    for (int i = 0; i < h->no_of_sections; i++)
+    {
+        printf("section%d: %s %d %d\n", i + 1, h->sectiuni[i].name, h->sectiuni[i].type, h->sectiuni[i].size);
     }
     free(h->sectiuni);
     free(h);
 }
 
+bool nr_line_of_section(char *path, header *h, int section)
+{
+    int fd = open(path, O_RDONLY);
+    if (fd == -1)
+    {
+        printf("ERROR\ninvalid path\n");
+        return false;
+    }
+    lseek(fd, h->sectiuni[section].offset, SEEK_SET);
+    int nr = 1;
+    char a, b;
+    read(fd, &a, 1);
+    for (unsigned int i = 1; i < h->sectiuni[section].size; i++)
+    {
+        read(fd, &b, 1);
+        if (i == h->sectiuni[section].size - 1)
+        {
+            break;
+        }
+        if (a == '\r' && b == '\n')
+        {
+            nr++;
+        }
+        if (nr > 15)
+        {
+            close(fd);
+            return false;
+        }
+        a = b;
+    }
+    close(fd);
+    if (nr == 15)
+    {
+        return true;
+    }
+    return false;
+}
+
+void extract(list *l)
+{
+    int n = 0;
+    header *h = parsare(l->path_string, &n);
+    if (h == NULL)
+    {
+        printf("ERROR\ninvalid file\n");
+        return;
+    }
+    if (l->section_nr > h->no_of_sections)
+    {
+        printf("ERROR\ninvalid section\n");
+        free(h->sectiuni);
+        free(h);
+        return;
+    }
+    int fd = open(l->path_string, O_RDONLY);
+    lseek(fd, h->sectiuni[l->section_nr - 1].offset, SEEK_SET);
+    int nr = 1;
+    char a, b;
+    int poz1 = 0, poz2 = 0;
+    read(fd, &a, 1);
+    if (l->line_nr == 1)
+    {
+        poz1 = h->sectiuni[l->section_nr - 1].offset;
+    }
+    for (unsigned int i = 1; i < h->sectiuni[l->section_nr - 1].size; i++)
+    {
+        read(fd, &b, 1);
+        if (a == '\r' && b == '\n')
+        {
+            nr++;
+        }
+        if (nr == l->line_nr && poz1 == 0)
+        {
+            poz1 = lseek(fd, 0, SEEK_CUR);
+        }
+        if (nr == (l->line_nr + 1))
+        {
+            poz2 = lseek(fd, 0, SEEK_CUR) - 2;
+            break;
+        }
+        a = b;
+    }
+    if (l->line_nr > nr)
+    {
+        printf("ERROR\ninvalid line\n");
+        close(fd);
+        free(h->sectiuni);
+        free(h);
+        return;
+    }
+    printf("SUCCESS\n");
+    for (int i = poz2; i >= poz1; i--)
+    {
+        lseek(fd, i, SEEK_SET);
+        read(fd, &a, 1);
+        printf("%c", a);
+    }
+    printf("\n");
+    close(fd);
+    free(h->sectiuni);
+    free(h);
+}
+
+void find_all(char *path, list *l, int ok)
+{
+    DIR *dir = NULL;
+    struct dirent *entry = NULL;
+    struct stat statbuf;
+    dir = opendir(path);
+    char filePath[512];
+    if (dir == NULL)
+    {
+        printf("ERROR\ninvalid directory path\n");
+        return;
+    }
+    if (ok == 0)
+    {
+        printf("SUCCESS\n");
+        ok++;
+    }
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
+        sprintf(filePath, "%s/%s", path, entry->d_name);
+        if (lstat(filePath, &statbuf) == 0)
+        {
+            if (S_ISDIR(statbuf.st_mode))
+            {
+                find_all(filePath, l, ok);
+            }
+            else
+            {
+                int n = 0;
+                header *h = parsare(filePath, &n);
+                if (h == NULL)
+                {
+                    continue;
+                }
+                int verif = 0;
+                if (h->no_of_sections >= 2)
+                {
+                    for (int i = 0; i < h->no_of_sections; i++)
+                    {
+                        if (nr_line_of_section(filePath, h, i))
+                        {
+                            verif++;
+                        }
+                        if (verif == 2)
+                        {
+                            printf("%s\n", filePath);
+                            break;
+                        }
+                    }
+                }
+                free(h->sectiuni);
+                free(h);
+            }
+        }
+    }
+    closedir(dir);
+}
 
 int main(int argc, char **argv)
 {
@@ -317,30 +429,64 @@ int main(int argc, char **argv)
         else
         {
             list *l = parsareArgumente(argc, argv);
-            int ok;
+            // list
             if (l->list)
             {
-                if (l->path)
+                listare(l->path_string, l, 1);
+            }
+            // parse
+            else if (l->parse)
+            {
+                header *h;
+                int eroare = 0;
+                h = parsare(l->path_string, &eroare);
+                if (h == NULL)
                 {
-                    ok = listare(l->path_string, l, 1);
-                    if (ok != 0)
+                    switch (eroare)
                     {
+                    case MAGIC:
+                    {
+                        printf("ERROR\nwrong magic\n");
+                        break;
+                    }
+                    case VERSION:
+                    {
+                        printf("ERROR\nwrong version\n");
+                        break;
+                    }
+                    case SECT_NR:
+                    {
+                        printf("ERROR\nwrong sect_nr\n");
+                        break;
+                    }
+                    case TYPES:
+                    {
+                        printf("ERROR\nwrong sect_types\n");
+                        break;
+                    }
+                    default:
                         printf("ERROR\ninvalid directory path\n");
                     }
                 }
                 else
                 {
-                    printf("ERROR\ninvalid directory path\n");
+                    print_header(h);
                 }
-            }else if(l->parse){
-                 parsare(l);
-                 
+            }
+            // extract
+            else if (l->extract)
+            {
+                extract(l);
+            }
+            // findall
+            else if (l->findall)
+            {
+                find_all(l->path_string, l, 0);
             }
             free(l->path_string);
             free(l->string);
             free(l);
         }
-        
     }
     return 0;
 }
