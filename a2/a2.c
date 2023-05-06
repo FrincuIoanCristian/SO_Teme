0645f46242da7c7c3c549f336f2a1749d4f64314
Frincu Ioan <ioan.frincu@yahoo.com>
2023-05-06 21:10:56 +0300

    Tema 2->commit 3

--- a2/a2.c
+++ a2/a2.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdbool.h>
 
 typedef struct
 {
@@ -15,47 +16,108 @@ typedef struct
     int number_proces;
 } TH_STRUCT;
 
-sem_t sem1_1, sem1_2, sem2;
+
+//semafoarele anonime pentru ex 2.3
+sem_t sem1_1, sem1_3;
+
+//semafoarele cu nume pentru ex 2.5
 sem_t *sem6_5, *sem6_2;
-// int nr_thread_2;
+
+//variabilele globale, semaforul anonim, lacatul si condiitiile pentru ex 2.4
+int nr_thread_block;
+bool T2_13_close;
+sem_t sem2;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond2 = PTHREAD_COND_INITIALIZER;
 
 void *thread_function(void *arg)
 {
     TH_STRUCT *s = (TH_STRUCT *)arg;
-    if (s->number_proces == 2)
+    if (s->number_proces == 3 && s->number_thread == 3) 
     {
-        sem_wait(&sem2);
+        sem_wait(&sem1_1);  //daca suntem pe thread-ul T3.3 asteptam sa inceapa mai intai T3.1
     }
-    if(s->number_proces == 3 && s->number_thread == 3){
-        sem_wait(&sem1_1);
-    }
-    if(s->number_proces == 3 && s->number_thread == 4){
+    if (s->number_proces == 3 && s->number_thread == 4)
+    {
         sem_wait(sem6_2);
     }
-    if(s->number_proces == 6 && s->number_thread == 5){
+    if (s->number_proces == 6 && s->number_thread == 5)
+    {
         sem_wait(sem6_5);
     }
     info(BEGIN, s->number_proces, s->number_thread);
 
-    if(s->number_proces == 3 && s->number_thread == 1){
-        sem_post(&sem1_1);
-        sem_wait(&sem1_2);
+    if (s->number_proces == 3 && s->number_thread == 1)
+    {
+        sem_post(&sem1_1);  //am pornit T3.1 si trezim T3.3
+        sem_wait(&sem1_3);  //daca suntem pe thread-ul T3.1 asteptam sa se termine mai intai T3.3
     }
 
     info(END, s->number_proces, s->number_thread);
-    if(s->number_proces == 3 && s->number_thread == 3){
-        sem_post(&sem1_2);
-    }
-    if (s->number_proces == 2)
+    if (s->number_proces == 3 && s->number_thread == 3)
     {
-        sem_post(&sem2);
+        sem_post(&sem1_3);  //am inchis T3.3 si trezim T3.1
     }
-    if(s->number_proces == 6 && s->number_thread == 2){
+    if (s->number_proces == 6 && s->number_thread == 2)
+    {
         sem_post(sem6_2);
     }
-    if(s->number_proces == 3 && s->number_thread == 4){
+    if (s->number_proces == 3 && s->number_thread == 4)
+    {
         sem_post(sem6_5);
     }
+    return NULL;
+}
+
+
+//->pentru ex 2.4 am facut o functie de thread separata pentru a-mi fi mai usor de observat 
+//->pentru rezolvare am ales un semafor anonim care sa permita maxim 5 thread-uri sa ruleze simultan
+//->pentru ultimul sub-punct am ales sa pornesc mai intai primele 4 thread-uri si thread-ul 13, restul le pun sa astepte pana cand voi inchide 
+//thread-ul special, adica 13. Pentru primele 4 thread-uri le pornesc si le pun si pe ele sa astepte. In thread-ul 13 astept pana cand primele
+//4 thread-uri au fost pornite dupa care afisez "END", si trezesc toate thread-urile blocate de cond si cond2 si le las sa isi execute in 
+//continuare executia in mod normal
+
+void *thread_function_process_2(void *param)
+{
+    int number_thread = *(int *)param;
+
+    if(T2_13_close == false && number_thread > 4 && number_thread != 13){
+        pthread_mutex_lock(&lock);
+        pthread_cond_wait(&cond2, &lock);
+        pthread_mutex_unlock(&lock);
+    }
+    sem_wait(&sem2);
+
+    info(BEGIN, 2, number_thread);
+
+    if (number_thread < 5)
+    {
+        pthread_mutex_lock(&lock);
+        nr_thread_block++;
+        pthread_cond_wait(&cond, &lock);
+        pthread_mutex_unlock(&lock);
+    }
+
+    if (number_thread == 13)
+    {
+        while (nr_thread_block < 4)
+        {
+            continue;
+        }
+    }
+
+    info(END, 2, number_thread);
+
+    if (number_thread == 13)
+    {
+        T2_13_close = true;
+        pthread_mutex_lock(&lock);
+        pthread_cond_broadcast(&cond);
+        pthread_cond_broadcast(&cond2);
+        pthread_mutex_unlock(&lock);
+    }
+    sem_post(&sem2);
     return NULL;
 }
 
@@ -66,7 +128,7 @@ int main()
     info(BEGIN, 1, 0);
     sem_init(&sem2, 0, 5);
     sem_init(&sem1_1, 0, 0);
-    sem_init(&sem1_2, 0, 0);
+    sem_init(&sem1_3, 0, 0);
     sem_unlink("sem6_2");
     sem_unlink("sem6_5");
     sem6_2 = sem_open("/sem6_2", O_CREAT, 0644, 0);
@@ -76,12 +138,13 @@ int main()
     {
         info(BEGIN, 2, 0);
         pthread_t T2[39];
-        TH_STRUCT s2[39];
+        int thread[39];
+        T2_13_close = false;
         for (int i = 0; i < 39; i++)
         {
-            s2[i].number_proces = 2;
-            s2[i].number_thread = i + 1;
-            pthread_create(&T2[i], NULL, thread_function, &s2[i]);
+
+            thread[i] = i + 1;
+            pthread_create(&T2[i], NULL, thread_function_process_2, &thread[i]);
         }
 
         for (int i = 0; i < 39; i++)
@@ -89,10 +152,12 @@ int main()
             pthread_join(T2[i], NULL);
         }
         sem_destroy(&sem2);
+        pthread_mutex_destroy(&lock);
+        pthread_cond_destroy(&cond);
+        pthread_cond_destroy(&cond2);
         if (fork() == 0)
         {
             info(BEGIN, 3, 0);
-            // creare thread-uri pentru procesul 3
             pthread_t T3[4];
             TH_STRUCT s3[4];
             for (int i = 0; i < 4; i++)
@@ -106,7 +171,7 @@ int main()
                 pthread_join(T3[i], NULL);
             }
             sem_destroy(&sem1_1);
-            sem_destroy(&sem1_2);
+            sem_destroy(&sem1_3);
             info(END, 3, 0);
             exit(0);
         }
